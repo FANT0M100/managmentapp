4a20be5a7754ea2c7030a110c701aff9ce857618
FANT0M100 <giorgie103188@gmail.com>
2024-02-12 16:25:38 +0400

    styling project sidebar. add new project component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ProjectsSidebar from "./component/ProjectsSidebar";
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8">
       <ProjectsSidebar />
     </main>
   );
