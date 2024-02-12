07ee8db0807b3f86cd1140e8077adb1b8fed348b
FANT0M100 <giorgie103188@gmail.com>
2024-02-12 17:50:46 +0400

    project details

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,15 @@ function App() {
     });
   }
 
+  function handleCancelAddproject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
       const newProject = {
@@ -36,7 +45,9 @@ function App() {
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddproject} />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
