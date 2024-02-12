17fc2e2d04123fdd058522dc58e018853b88b350
FANT0M100 <giorgie103188@gmail.com>
2024-02-12 16:19:52 +0400

    add project sidebar component

---
+++ src/component/ProjectsSidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProjectsSidebar = () => {
+  return (
+    <aside>
+      <h1>Your Projects</h1>
+      <div>
+        <button>+ Add project</button>
+      </div>
+    </aside>
+  );
+};
+
+export default ProjectsSidebar;
