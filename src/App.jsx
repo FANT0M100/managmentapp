979de1492e86d785c77d8e78cdf0bdf5ea84f2df
FANT0M100 <giorgie103188@gmail.com>
2024-02-12 16:55:51 +0400

    add no project selected component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./component/NewProject";
+import NoProjectSelected from "./component/NoProjectSelected";
 import ProjectsSidebar from "./component/ProjectsSidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
